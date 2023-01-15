7c2fa1f7897facfb37b5cc17ecae46dbfbef39e1
gabrielagabriela <gabrielaa.slv@gmail.com>
2023-01-15 20:17:01 -0300

    ex03_sem03

--- src/controllers/solicitation.controller.js
+++ src/controllers/solicitation.controller.js
@@ -1,13 +1,35 @@
-import { v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
+import { getSolicitationsInFile } from '../utils/getSolicitationsInFile.js'
+import fs from 'fs'
+import { readFileJson } from '../utils/readFileJson.js'
 
 export function findMany(request, response) {
-  response.json([])
+  const solicitations = getSolicitationsInFile()
+  response.json(solicitations)
 }
 
-export function create(request, response){
-  const {name_client, document_client, contact_client, address_client, payment_method, observations, pizzas} = request.body;
+export function findOne(request, response) {
+  const { id } = request.params
 
-  const solicitation = {
+  const solicitations = readFileJson('solicitations.json')
+
+  const solicitation = solicitations.find(solicitation => solicitation.id === id)
+
+  return response.json(solicitation)
+}
+
+export function create(request, response) {
+  const {
+    name_client,
+    document_client,
+    contact_client,
+    address_client,
+    payment_method,
+    observations,
+    pizzas
+  } = request.body
+
+  const soliciation = {
     id: uuidv4(),
     name_client,
     document_client,
@@ -16,15 +38,33 @@ export function create(request, response){
     payment_method,
     observations,
     pizzas,
-    status: "Em Produção"
+    order: "EM PRODUÇÃO"
   }
 
-  solicitations.push(solicitation)
-  response.status(201).json(solicitation);
+  const solicitations = getSolicitationsInFile()
+
+  fs.writeFileSync('solicitations.json', JSON.stringify([...solicitations, soliciation]))
+
+  response.status(201).json(soliciation)
 }
 
-export function findOne(request, response){
-  const {id} = request.params
-  const solicitation = solicitations.find(solicitation => solicitation.id == id)
-  return response.json(solicitation)
+{
+
+}
+
+
+export function updateStatus(request, response) {
+  const solicitations = getSolicitationsInFile()
+
+ const updatedSolicitations = solicitations.map(solicitation => {
+    if (solicitation.id === request.params.id) {
+      solicitation.order = 'HÁ CAMINHO'
+    }
+    return solicitation
+  })
+
+  fs.writeFileSync('solicitations.json', JSON.stringify(updatedSolicitations))
+
+  return response.json()
+
 }
